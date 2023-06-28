29af093d123dba2c2d878dabc582e0a4f5b8faad
Varun Yadav <Varun Yadav@VARUN>
2023-06-28 06:40:32 +0530

    Add project files.

---
+++ AcmeCorpCustomerManagementAPI/Data/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using AcmeCorpCustomerManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcmeCorpCustomerManagementAPI.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ AcmeCorpCustomerManagementAPI/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcmeCorpCustomerManagementAPI.Models
+{
+    [Table("customers")]
+    public class Customer
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("name")]
+        public required string Name { get; set; }
+
+        [Column("email")]
+        public required string Email { get; set; }
+    }
+}
